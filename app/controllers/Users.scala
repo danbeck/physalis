826cb0a5ff7d519b0b0f03b9a6a395c870c8cfb5
Daniel Beck <d.danielbeck@googlemail.com>
2015-01-09 21:28:05 +0100

    Added user-template.

--- app/controllers/Users.scala
+++ app/controllers/Users.scala
@@ -3,13 +3,12 @@ package controllers
 import play.api.mvc.Controller
 import play.api.mvc.RequestHeader
 import securesocial.core.SecureSocial
+import play.api.mvc.Action
 
 object Users extends Controller with SecureSocial {
 
-  def index(username: String) = SecuredAction { implicit requestHeader: RequestHeader =>
-    JavaContext.withContext {
-      Ok("Hello " + username )
-    }
+  def index(username: String) = Action { implicit requestHeader: RequestHeader =>
+    Ok(views.html.user(null))
   }
 
 }
