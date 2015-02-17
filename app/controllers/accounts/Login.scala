2a388bd20f2a3d2cec1cad449e3fa6b81aa93f5e
Daniel Beck <d.danielbeck@googlemail.com>
2015-02-17 18:15:58 +0100

    Added documentation.

--- app/controllers/accounts/Login.scala
+++ app/controllers/accounts/Login.scala
@@ -5,11 +5,15 @@ import models.User
 import play.api.mvc.Action
 import play.api.i18n.Messages
 
+/**
+ * Login and Signup are controlled by the same workflow: if a user logs in for his first time ever in physalis,
+ * a new account is created. If a user tries to signup with an actually existing account, he is logged in.
+ */
 class Login(override implicit val env: RuntimeEnvironment[User]) extends securesocial.core.SecureSocial[User] {
   def login = Action { implicit request =>
     Ok(views.html.login(null))
   }
-  
+
   def loginRedirectURI = UserAwareAction { implicit request =>
     request.user match {
       case Some(u) if !u.newUser => Redirect(routes.Index.user(u.username.get))
@@ -22,7 +26,6 @@ class Login(override implicit val env: RuntimeEnvironment[User]) extends secures
     Redirect(controllers.routes.CustomLoginController.logout).flashing("success" -> Messages("youve.been.logged.out"))
   }
 
-  
   def signup = UserAwareAction { implicit request =>
     Ok(views.html.accounts.signup(null))
   }
