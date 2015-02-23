51b1d2696b7b1fe5de32a85ec957f4c71364eb01
Daniel Beck <d.danielbeck@googlemail.com>
2015-02-23 21:39:20 +0100

    Refactoring

--- app/controllers/accounts/Index.scala
+++ app/controllers/accounts/Index.scala
@@ -16,33 +16,25 @@ import scala.util.Failure
 
 class Index(override implicit val env: RuntimeEnvironment[User]) extends securesocial.core.SecureSocial[User] {
 
-  def index = UserAwareAction { implicit request =>
-    request.user match {
-      case Some(user) => Ok(views.html.accounts.index(user))
-      case None       => Ok(views.html.accounts.pleaseLogin())
-    }
-  }
-
-  def findUser(username: String): Future[Option[User]] = {
-    env.userService.asInstanceOf[UpdatableUserService].find(username)
-  }
+  val USER_SERVICE = env.userService.asInstanceOf[UpdatableUserService]
 
   def user(username: String) = UserAwareAction.async { implicit request =>
-    request.user match { //
-      case Some(u) if u.username.get == username => Future { Ok(views.html.accounts.index(u)) }
-      case _ => {
-        val futureUser = findUser(username)
-        futureUser.map {
-          case Some(user) => Ok(views.html.accounts.index(user))
-          case None       => Ok("This user doesn't exist")
-        }
+    def findUser(username: String) = USER_SERVICE.find(username)
+    def showMyAccount(user: User) = Future { Ok(views.html.accounts.index(user)) }
+    def showPublicAccount(username: String) = {
+      val userFuture = findUser(username)
+      userFuture.map {
+        case Some(user) => Ok(views.html.accounts.index(user))
+        case None       => Ok("This user doesn't exist")
       }
     }
-  }
 
-  //  def logout = Action {
-  //    Redirect("/logout").flashing("success" -> Messages("youve.been.logged.out"))
-  //  }
+    request.user match { //
+      case Some(u) if u.username.get == username => showMyAccount(u)
+      case Some(u)                               => showPublicAccount(username)
+      case None                                  => showPublicAccount(username)
+    }
+  }
 
   def login = Action { implicit request =>
     Ok(views.html.login(null))
