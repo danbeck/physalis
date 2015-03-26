91cb3d0fbea8d0d3477fb2e9ba572a494bada605
Daniel Beck <d.danielbeck@googlemail.com>
2015-03-26 22:37:37 +0100

    SimpleDBUserService: new find()-method.

--- app/service/SimpleDBUserService.scala
+++ app/service/SimpleDBUserService.scala
@@ -1,5 +1,6 @@
 package service
 
+import play.api.Logger
 import awscala.simpledb.SimpleDBClient
 import awscala.simpledb.Domain
 import models.User
@@ -11,13 +12,15 @@ import securesocial.core.services.SaveMode
 import play.api.Logger
 import java.util.UUID
 import models.Project
+import awscala.simpledb.Item
+import securesocial.core.BasicProfile
 
 class SimpleDBUserService extends UpdatableUserService {
 
   implicit val simpleDB = new SimpleDBClient()
-  val usersDomain: Domain = simpleDB.createDomain("user")
-  val projectsDomain: Domain = simpleDB.createDomain("project")
-  val basicProfileDomain: Domain = simpleDB.createDomain("basicProfile")
+  val usersDomain = simpleDB.createDomain("user")
+  val projectsDomain = simpleDB.createDomain("project")
+  val basicProfileDomain = simpleDB.createDomain("basicProfile")
 
   def save(project: Project) = {
     Logger.info(s"SimpleDB: Adding project '${project.id}' '${project.name}' '${project.gitUrl}'")
@@ -29,80 +32,75 @@ class SimpleDBUserService extends UpdatableUserService {
       "s3Url" -> "")
 
   }
-  //  def saveUser(user: User) = {
-  //    Logger.info("creating simpledb")
-  //    implicit val simpleDB = new SimpleDBClient()
-  //    //    val region: Region = new Region()
-  //    Logger.info("creating domain")
-  //    val domain: Domain = simpleDB.createDomain("users")
-  //    domain.put("daniel", "name" -> "Daniel Beck", "email" -> "d.danielbeck@googlemail.com")
-  //    domain.put("karin", "name" -> "Karin Beck", "email" -> "kbeck@googlemail.com")
-  //
-  //    val items: Seq[Item] = domain.select(s"select * from users where name = 'Daniel Beck'")
-  //
-  //    Logger.info("items: " + items)
-  //    Logger.error("items: " + items)
-  //    simpleDB.domains.foreach(_.destroy())
-  //
-  //  }
-  // Members declared in service.UpdatableUserService
+
   def find(username: String): Future[Option[User]] = {
     Logger.info(s"find '$username")
     null
   }
+
   def projects: List[models.Project] = { List() }
+
   def update(user: models.User): models.User = { null }
 
   def deleteExpiredTokens(): Unit = {}
+
   def deleteToken(uuid: String): Future[Option[MailToken]] = { null }
-  def find(providerId: String, userId: String): Future[Option[BasicProfile]] = {
-    Logger.info(s"find '$providerId' '$userId")
-    Future.successful(None)
+
+  def find(providerId: String, profileId: String): Future[Option[BasicProfile]] = {
+    Logger.info(s"find '$providerId' '$profileId")
+
+    //    val items: Seq[Item] = basicProfileDomain.select(s"select * from basicProfile where providerId = ${providerId} and profileId = ${profileId}")
+    val items: Seq[Item] = basicProfileDomain.select(s"select id, providerId, profileId, userId from basicProfile where providerId = '${providerId}' and profileId = '${profileId}'")
+    items.headOption match {
+      case Some(item) =>
+        val providerId = item.attributes(0).value
+        val profileId = item.attributes(1).value
+        Logger.info(s"found: providerId: ${providerId}, profileId: ${profileId}")
+        val profile = BasicProfile(providerId = providerId,
+          userId = profileId,
+          firstName = None,
+          lastName = None,
+          fullName = None,
+          email = None,
+          avatarUrl = None,
+          authMethod = null,
+          oAuth1Info = None,
+          oAuth2Info = None,
+          passwordInfo = None)
+
+        Future.successful(Some(profile))
+      case None => Future.successful(None)
+    }
   }
 
   def findByEmailAndProvider(email: String, providerId: String): Future[Option[BasicProfile]] = {
     Logger.info(s"findByEmail '$providerId' '$email")
     null
   }
+
   def findToken(token: String): Future[Option[MailToken]] = { null }
+
   def link(current: User, to: BasicProfile): Future[User] = { null }
+
   def passwordInfoFor(user: User): Future[Option[PasswordInfo]] = { null }
+
   def save(profile: BasicProfile, mode: SaveMode): Future[models.User] = {
-    val newUser: User = mode match {
-      case SaveMode.SignUp         => User(id = UUID.randomUUID().toString, main = profile, identities = List(profile))
-      case SaveMode.LoggedIn       => null
-      case SaveMode.PasswordChange => null
-    }
-    saveNewUser()
+    val newUser = User(id = UUID.randomUUID().toString, main = profile, identities = List(profile))
 
-    def saveNewUser() = {
-      val profileid = UUID.randomUUID().toString()
-      //      
-      //      providerId = "github",
-      //        userId = "karin",
-      //        firstName = Some("Karin"),
-      //        lastName = Some("Beck"),
-      //        fullName = Some("Karin Beck"),
-      //        email = Some("karin@freen.et"),
-      //        avatarUrl = Some("www"),
-      //        authMethod = null,
-      //        oAuth1Info = None,
-      //        oAuth2Info = None,
-      //        passwordInfo = None)
-      //        
-      //        
+    val profileid = UUID.randomUUID().toString()
+    basicProfileDomain.put(profileid,
+      "providerId" -> profile.providerId,
+      "profileId" -> profile.userId,
+      "firstName" -> profile.firstName.getOrElse(null),
+      "lastName" -> profile.lastName.getOrElse(null),
+      "email" -> profile.email.getOrElse(null),
+      "avatarUrl" -> profile.avatarUrl.getOrElse(null))
 
-      basicProfileDomain.put(profileid,
-        "providerId" -> profile.providerId,
-        "userId" -> profile.userId,
-        "firstName" -> profile.firstName.getOrElse(null),
-        "lastName" -> profile.lastName.getOrElse(null),
-        "email" -> profile.email.getOrElse(null),
-        "avatarUrl" -> profile.avatarUrl.getOrElse(null))
-    }
-    //    usersDomain.put(newUser.id, "username" -> newUser.username, "fullname" -> newUser.fullname, "email" -> newUser.email)
+    find(profile.providerId, profile.userId)
     Future.successful(newUser)
   }
+
   def saveToken(token: MailToken): Future[MailToken] = { null }
+
   def updatePasswordInfo(user: models.User, info: PasswordInfo): Future[Option[BasicProfile]] = { null }
 }
