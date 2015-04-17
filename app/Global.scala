80f4646a93284feb73ad1d935e3c60f4af5d42ee
danbeck <d.danielbeck@googlemail.com>
2015-04-17 22:50:33 +0200

    cleanup.

--- app/Global.scala
+++ app/Global.scala
@@ -9,7 +9,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,82 +18,27 @@
  * limitations under the License.
  *
  */
-import controllers.CustomRoutesService
+
 import java.lang.reflect.Constructor
-import securesocial.core.RuntimeEnvironment
-import service.{ SecureSocialEventListener, InMemoryUserService }
-import scala.collection.immutable.ListMap
-import securesocial.core.providers.GitHubProvider
-import service.SecureSocialEventListener
-import securesocial.controllers.ViewTemplates
+
+import controllers.CustomRoutesService
 import models.User
-import play.api.data.Form
-import play.api.mvc.RequestHeader
-import play.api.i18n.Lang
-import play.twirl.api.Html
-import play.api.mvc.Flash
-import securesocial.controllers.RegistrationInfo
-import securesocial.controllers.ChangeInfo
-import securesocial.core.services.UserService
 import play.Play
-import service.simpledb.{ UserService => SimpleDBUserService }
+import play.api.data.Form
+import play.api.i18n.Lang
+import play.api.mvc.RequestHeader
+import securesocial.views.html.Registration
+import play.twirl.api.Html
+import securesocial.controllers.{ChangeInfo, RegistrationInfo, ViewTemplates}
+import securesocial.core.RuntimeEnvironment
+import securesocial.core.providers.GitHubProvider
+import securesocial.core.services.UserService
+import service.{InMemoryUserService, SecureSocialEventListener}
+import service.simpledb.{UserService => SimpleDBUserService}
+
+import scala.collection.immutable.ListMap
 
 object Global extends play.api.GlobalSettings {
-  object MyViewTemplates {
-    /**
-     * The default views.
-     */
-    class Default(env: RuntimeEnvironment[_]) extends ViewTemplates {
-      implicit val implicitEnv = env
-      override def getLoginPage(form: Form[(String, String)],
-                                msg: Option[String] = None)(implicit request: RequestHeader, lang: Lang): Html = {
-        views.html.login(null)
-      }
-      override def getSignUpPage(form: Form[RegistrationInfo], token: String)(implicit request: RequestHeader, lang: Lang): Html = {
-        securesocial.views.html.Registration.signUp(form, token)
-      }
-      override def getStartSignUpPage(form: Form[String])(implicit request: RequestHeader, lang: Lang): Html = {
-        securesocial.views.html.Registration.startSignUp(form)
-      }
-      override def getStartResetPasswordPage(form: Form[String])(implicit request: RequestHeader, lang: Lang): Html = {
-        securesocial.views.html.Registration.startResetPassword(form)
-      }
-      override def getResetPasswordPage(form: Form[(String, String)], token: String)(implicit request: RequestHeader, lang: Lang): Html = {
-        securesocial.views.html.Registration.resetPasswordPage(form, token)
-      }
-      override def getPasswordChangePage(form: Form[ChangeInfo])(implicit request: RequestHeader, lang: Lang): Html = {
-        securesocial.views.html.passwordChange(form)
-      }
-      def getNotAuthorizedPage(implicit request: RequestHeader, lang: Lang): Html = {
-        Html("no authorized")
-      }
-    }
-  }
-  /**
-   * The runtime environment for this sample app.
-   */
-  object MyRuntimeEnvironment extends RuntimeEnvironment.Default[User] {
-    override lazy val routes = new CustomRoutesService()
-    override lazy val userService: UserService[User] = createUserService()
-    override lazy val eventListeners = List(new SecureSocialEventListener())
-    //    override lazy val viewTemplates: ViewTemplates = new ViewTemplates.Default(this)
-    override lazy val viewTemplates: ViewTemplates = new MyViewTemplates.Default(this)
-    override lazy val providers = ListMap(
-      include(new GitHubProvider(routes, cacheService, oauth2ClientFor(GitHubProvider.GitHub))))
-    //      ,include(new GoogleProvider(routes, cacheService, oauth2ClientFor(GoogleProvider.Google))),
-    //      include(new UsernamePasswordProvider[DemoUser](userService, avatarService, viewTemplates, passwordHashers)))
-
-    private def createUserService() = {
-      val inMemoryDB: Boolean = Play.application.configuration.getBoolean("IN_MEMORY_DB", false)
-
-      inMemoryDB match {
-        case true  => new InMemoryUserService
-        case false => new SimpleDBUserService
-      }
-
-    }
-
-  }
 
   /**
    * An implementation that checks if the controller expects a RuntimeEnvironment and
@@ -113,5 +58,71 @@ object Global extends play.api.GlobalSettings {
       _.asInstanceOf[Constructor[A]].newInstance(MyRuntimeEnvironment)
     }
     instance.getOrElse(super.getControllerInstance(controllerClass))
+  }
+
+  object MyViewTemplates {
+
+    /**
+     * The default views.
+     */
+    class Default(env: RuntimeEnvironment[_]) extends ViewTemplates {
+      implicit val implicitEnv = env
+
+      override def getLoginPage(form: Form[(String, String)],
+                                msg: Option[String] = None)
+                               (implicit request: RequestHeader, lang: Lang): Html =
+        views.html.login(null)
+
+      override def getSignUpPage(form: Form[RegistrationInfo], token: String)
+                                (implicit request: RequestHeader, lang: Lang): Html =
+        Registration.signUp(form, token)
+
+      override def getStartSignUpPage(form: Form[String])
+                                     (implicit request: RequestHeader, lang: Lang): Html =
+        Registration.startSignUp(form)
+
+      override def getStartResetPasswordPage(form: Form[String])(implicit request: RequestHeader, lang: Lang): Html =
+        Registration.startResetPassword(form)
+
+
+      override def getResetPasswordPage(form: Form[(String, String)], token: String)(implicit request: RequestHeader, lang: Lang): Html =
+        Registration.resetPasswordPage(form, token)
+
+
+      override def getPasswordChangePage(form: Form[ChangeInfo])(implicit request: RequestHeader, lang: Lang): Html =
+        securesocial.views.html.passwordChange(form)
+
+
+      def getNotAuthorizedPage(implicit request: RequestHeader, lang: Lang): Html =
+        Html("no authorized")
+
+    }
+
+  }
+
+  /**
+   * The runtime environment for this sample app.
+   */
+  object MyRuntimeEnvironment extends RuntimeEnvironment.Default[User] {
+    override lazy val routes = new CustomRoutesService()
+    override lazy val userService: UserService[User] = createUserService()
+    override lazy val eventListeners = List(new SecureSocialEventListener())
+    //    override lazy val viewTemplates: ViewTemplates = new ViewTemplates.Default(this)
+    override lazy val viewTemplates: ViewTemplates = new MyViewTemplates.Default(this)
+    override lazy val providers = ListMap(
+      include(new GitHubProvider(routes, cacheService, oauth2ClientFor(GitHubProvider.GitHub))))
+    //      ,include(new GoogleProvider(routes, cacheService, oauth2ClientFor(GoogleProvider.Google))),
+    //      include(new UsernamePasswordProvider[DemoUser](userService, avatarService, viewTemplates, passwordHashers)))
+
+    private def createUserService() = {
+      val inMemoryDB: Boolean = Play.application.configuration.getBoolean("IN_MEMORY_DB", false)
+
+      inMemoryDB match {
+        case true => new InMemoryUserService
+        case false => new SimpleDBUserService
+      }
+
+    }
+
   }
 }
