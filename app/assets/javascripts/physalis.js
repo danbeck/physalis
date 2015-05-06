1387598358e3163ab11de598972732027884bf81
danbeck <d.danielbeck@googlemail.com>
2015-05-06 19:38:15 +0200

    Added javascript to check the state of the last build.

--- app/assets/javascripts/physalis.js
+++ app/assets/javascripts/physalis.js
@@ -1,20 +1,44 @@
 $(document).ready(
 		function() {
 
-			$("#example p:first").css("display", "block");
+			function retrieveAndAddBuildDataToDom(node$) {
+				var projectname = node$.attr("data-project");
+				var username = node$.attr("data-user");
 
-			jQuery.fn.timer = function() {
-				if (!$(this).children("p:last-child").is(":visible")) {
-					$(this).children("p:visible").css("display", "none").next(
-							"p").css("display", "block");
-				} else {
-					$(this).children("p:visible").css("display", "none").end()
-							.children("p:first").css("display", "block");
-				}
-			};
+				var result = {};
+				$.getJSON("latest/" + username + "/" + projectname, function(
+						data) {
+					if (!data.error) {
+						node$.empty();
+						if (data.android.state === "NEW") {
+							node$.append("<p>Android: build queued</p>");
+						}
+						if (data.ubuntu.state === "NEW") {
+							node$.append("<p>Ubuntu: build queued</p>");
+						}
+						if (data.android.state === "IN_PROGRESS") {
+							node$.append("<p>Android build in progress</p>");
+						}
+						if (data.ubuntu.state === "IN_PROGRESS") {
+							node$.append("<p>Ubuntu build in progress</p>");
+						}
+						if (data.android.state === "DONE") {
+							node$.append("<p><a class='btn btn-lg' href='" + data.android.url + "'>Download Android APK</a></p>");
+						}
+						if (data.ubuntu.state === "DONE") {
+							node$.append("<p><a class='btn btn-lg' href='" + data.ubuntu.url + "'>Download Android APK</a></p>");
+						}
+
+					}
+
+				});
+
+			}
 
 			window.setInterval(function() {
-				$("#example").timer();
+				$("div[data-role='downloadProjects']").each(function() {
+					retrieveAndAddBuildDataToDom($(this));
+				});
 			}, 1000);
 
 		});
